d0d6f6e404af0e38fa96cd3405654e80942efabb
daksh123456 <dakshkanaujia@gmail.com>
2024-07-12 17:40:53 +0530

    Jul 12

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,45 +1,111 @@
 const express = require("express");
 const User = require("../models/userModel");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try {
-        const userExists = await User.findOne({email : req.body.email})
-        if(userExists){
-            res.json("User Exists")
-        }
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        req.body.password = hashedPassword;
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
 
-        const newUser = new User(req.body)
-        await newUser.save()
-        console.log("ho gaya")
-        res.json("User Created")
-    } catch (error){
-        console.log("nai hua")
-        res.json(error);
+    if (userExists) {
+      res.send({
+        success: false,
+        message: "User Already Exists",
+      });
+      res.json("User Exists")
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.send({
+      success: true,
+      message: "You've successfully signed up, please login now!",
+    });
+    res.json("Succesfully Resgistered")
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({email : req.body.email})
-        if(!user){
-            res.json("User not found")
-        }
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-        if(!validPassword){
-            res.json("Invalid Password")
-        }
-        res.json("Logged In")
-    } catch (error){
-        res.json(error);
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      res.send({
+        success: false,
+        message: "User Does not exist , please register",
+      });
+      res.json("User Does Not Exist")
     }
-  
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      res.send({
+        success: false,
+        message: "Invalid Password",
+      });
+      res.json("Invalid Password")
+      return;
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.send({
+      success: true,
+      message: "You've successfully logged in!",
+      token: token,
+    });
+    res.json("Successfully Logged in")
+  } catch (error) {
+    console.error(error);
+  }
 });
 
+/*
 
+// router.get("/get-current-user", authMiddleware, async (req, res) => {
+//   const user = await User.findById(req.body.userId).select("-password");
+
+//   res.send({
+//     success: true,
+//     message: 'You are authorized to go to the protected route!',
+//     data: user
+//    })
+// });
+
+*/
+
+router.get("/get-current-user", authMiddleware , async(req, res) => {
+
+    try{
+
+        const user = await User.findById(req.body.userId).select('-password')
+        res.send({
+            success: true,
+            message: 'You are authorized to go to the protected route!',
+            data: user
+        })
+    }catch (err){
+        res.send({
+            success : false,
+            message : 'not authorized'
+        })
+        console.log(err)
+    }
+})
 module.exports = router;
